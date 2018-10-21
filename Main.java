9a1b061322b8bf8c5a755a31e561636d8f105df1
X900 <pz@iqff.org>
2018-10-20 21:53:27 -0700

    First java programme

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String message = "To jest mój pierwszy łańcuch znaków! Potocznie \"string\".";
+        System.out.println(message);
+    }
+}
+
